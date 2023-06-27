f80fc90469f8370fffe2d976f82ca78a127a03e4
vnikolaus <victor_nikolaus@hotmail.com>
2023-06-27 01:54:41 -0300

    commit_port_v

---
+++ ValidatorCPF/verifyCPF.js
@@ -0,0 +1,21 @@
+'use strict';
+function verifyCPF(cpf) {
+  cpf = String(cpf).replace(/\D/g, '');
+  if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+  let check = true;
+  [9, 10].forEach((pos) => {
+    let sum = 0,
+      res = 0;
+    cpf = String(cpf);
+    cpf
+      .split(/(?=)/)
+      .splice(0, pos)
+      .forEach((el, i) => {
+        sum += Number(el) * (pos + 2 - (i + 1));
+      });
+    res = sum % 11;
+    res = res < 2 ? 0 : 11 - res;
+    if (res != Number(cpf.substring(pos, pos + 1))) check = false;
+  });
+  return check;
+}
