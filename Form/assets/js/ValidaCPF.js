564d6c2e977280bdadfb1eefc15131dad3d7e253
vnikolaus <victor_nikolaus@hotmail.com>
2023-06-27 02:04:55 -0300

    commit_port_v

--- Form/assets/js/ValidaCPF.js
+++ Form/assets/js/ValidaCPF.js
@@ -1,40 +1,23 @@
 export default class ValidaCPF {
     
     valid(cpf) {
-        const _strCPF = this.ajust_cpf(cpf);
-        
-        const _clrCPF = String(cpf).replace(/\D+/g, '');
-
-        const secondLastDigit = this.generate_digit(_strCPF);
-        const lastDigit = this.generate_digit(_strCPF + secondLastDigit);
-
-        const verificationCpf = _strCPF + secondLastDigit + lastDigit;
-
-        return Number(_clrCPF === verificationCpf) ? true : false;
-    }
-
-    ajust_cpf(value) {
-        const _str = String(value);
-        const _clr = _str.replace(/\D+/g, '');
-
-        if (_clr.length === 9) return _clr;
-
-        const adjusted_cpf = _clr.substring(0, _clr.length - 2);
-        return adjusted_cpf;
-    }
-
-    generate_digit(val) {
-        let l = val.length + 1;
-        let sum = 0;
-
-        for (let num in val) {
-            sum += l * (val[num]);
-            l--;
-        }
-    
-        let newDigit = (11 - (sum % 11));
-            newDigit > 9 ? newDigit = 0 : newDigit;
-            
-        return newDigit;
-    }
+        cpf = String(cpf).replace(/\D/g, '');
+        if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) return false;
+        let check = true;
+        [9, 10].forEach((pos) => {
+          let sum = 0,
+            res = 0;
+          cpf = String(cpf);
+          cpf
+            .split(/(?=)/)
+            .splice(0, pos)
+            .forEach((el, i) => {
+              sum += Number(el) * (pos + 2 - (i + 1));
+            });
+          res = sum % 11;
+          res = res < 2 ? 0 : 11 - res;
+          if (res != Number(cpf.substring(pos, pos + 1))) check = false;
+        });
+        return check;
+      }
 }
