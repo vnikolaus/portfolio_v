90cff567c474a853374f217709d27724f3cc6e0d
vnikolaus <victor_nikolaus@hotmail.com>
2023-08-01 23:46:53 -0300

    commit_port_v

--- NestJS_API_Users/src/application/db/db.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Database } from 'src/application/db/db.datasource';
-
-@Module({
-  controllers: [],
-  providers: [Database],
-  exports: [Database]
-})
-export class DbModule {}
