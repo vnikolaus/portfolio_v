eb36908dc2b9df1eb959db942f3591b244a5b35a
vnikolaus <victor_nikolaus@hotmail.com>
2023-08-01 23:54:56 -0300

    commit_port_v

--- NestJS_API_Workflow_SIG/src/useCases/user/user.module.ts
+++ NestJS_API_Workflow_SIG/src/useCases/user/user.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { User } from 'src/entities/user.entity'
-import { DatabaseModule } from 'src/database/database.module'
+import { User } from '../../entities/user.entity'
+import { DatabaseModule } from '../../database/database.module'
 
 @Module({
     imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
