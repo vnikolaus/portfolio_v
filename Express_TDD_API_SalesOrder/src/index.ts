e11d623dc1b56f7df3d19dd5f10de975b24b0faa
vnikolaus <victor_nikolaus@hotmail.com>
2023-09-24 20:17:54 -0300

    commit_port_v

--- Express_TDD_API_SalesOrder/src/index.ts
+++ Express_TDD_API_SalesOrder/src/index.ts
@@ -14,7 +14,7 @@ app.use('/products', productRouter)
 
 app.use(errorMiddleware)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT ?? 3000
 app.listen(+PORT, () => {
     console.log(`API running at: http://localhost:${PORT}`)
 })
