4331f7d38327b214a6e605babca86b2dce8aea45
vnikolaus <victor_nikolaus@hotmail.com>
2024-02-04 10:20:53 -0300

    commit port_view

--- NodeJS/NodeJS_Consult_Stocks/src/controller/change_logo.js
+++
@@ -1,9 +0,0 @@
-async function changeLogo(symbol) {
-    if(symbol === 'SANB4') symbol = 'SANB11'
-    let logo = document.querySelector('.logo');
-
-    logo.hidden = false;
-    logo.setAttribute('src', `../src/components/img/${symbol}.svg`);
-}
-
-export default changeLogo;
