47134b6d0b9aaec5735b74844b9b1d1ac83fb73f
vnikolaus <victor_nikolaus@hotmail.com>
2024-03-23 23:00:03 -0300

    commit port_view

--- API 's/Express_APIv2_Book_Reservation/tests/units/Period.spec.ts
+++ API 's/Express_APIv2_Book_Reservation/tests/units/Period.spec.ts
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest'
 import { formatDistance } from 'date-fns'
 import { Period } from '../../src/domain/services/Period'
 
-describe('CALCULATE DATES - TEST', () => {
+describe('PERIOD - TEST', () => {
     it('Should calculate a correct dates, based on reserved duration', () => {
         const duration = 20
         const { start_date, end_date } = Period.generateDates(duration)
